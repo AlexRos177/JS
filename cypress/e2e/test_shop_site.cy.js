f25e64f0cc82f8b11c2ac1477a2a34d358d80f38
aleksros <ksiars604@gmail.com>
2024-08-05 17:02:15 +0300

    Add new data

--- cypress/e2e/test_shop_site.cy.js
+++ cypress/e2e/test_shop_site.cy.js
@@ -1,4 +1,4 @@
-describe('My First Test', () => {
+describe('Shop test/ log in and buy items', () => {
 
     it('Visits the shopsite and buy item', () => {
 
@@ -29,10 +29,10 @@ describe('My First Test', () => {
         cy.get('[data-test="back-to-products"]').click();
         cy.get('#react-burger-menu-btn').click();
         cy.get('#logout_sidebar_link').contains('Logout').click();
-            
-        });
 
-    it.only('Visits the shopsite and buy item', () => {
+    });
+
+    it('Visits the shopsite and buy item', () => {
 
         cy.visit('https://www.saucedemo.com/');
 
@@ -43,10 +43,85 @@ describe('My First Test', () => {
         // Submit the form
         cy.get('#login-button').click();
 
+        // Assert that the login was not successful
+        cy.get('#login_button_container > div > form > div.error-message-container.error');
+
+    });
+
+    it('Visits the shopsite and buy item', () => {
+
+        cy.visit('https://www.saucedemo.com/');
+
+        // Enter the extracted text into the authentication form
+        cy.get('#user-name').type('problem_user');
+        cy.get('#password').type('secret_sauce');
+
+        // Submit the form
+        cy.get('#login-button').click();
+
         // Assert that the login was successful
         cy.url().should('include', '/inventory.html');
-    
-    
+
+        cy.get('#add-to-cart-sauce-labs-backpack').click();
+
+        cy.get('[data-test="shopping-cart-link"]').click();
+        //cy.get('[data-test="shopping-cart-link"]').contains('Sauce_Labs_Backpack');
+        cy.get('#item_4_title_link > div').contains('Sauce Labs Backpack').click();
+        const itemName = 'Sauce Labs Backpack';
+        const expectedPrice = '$29.99';
+
+        // Check if the item with the specified name and price exists
+        cy.contains('Swag Labs').then(item => {
+            if (item.text == itemName) {
+                cy.log('Item found!');
+                // If the item is found, check the price
+                cy.wrap(item).parents('.inventory_item_price').should('have.text', expectedPrice);
+            } else {
+                cy.log('Item not found, navigating back.');
+                // If the item is not found, go back
+                cy.go('back');
+                //Remove the wrong item, and go back
+                cy.get('[data-test="remove-sauce-labs-backpack"]').click();
+                cy.get('[data-test="continue-shopping"]').click();
+            }
+        });
+
+    });
+
+    it('Visits the shopsite and buy item', () => {
+        cy.visit('https://www.saucedemo.com/');
+        // Enter the extracted text into the authentication form
+        cy.get('#user-name').type('performance_glitch_user');
+        cy.get('#password').type('secret_sauce');
+        // Submit the form
+        cy.get('#login-button').click();
+        // Assert that the login was successful
+        cy.url().should('include', '/inventory.html');
+        //Add item to the cart
+        cy.get('#add-to-cart-sauce-labs-backpack').click();
+        cy.get('[data-test="shopping-cart-link"]').click();
+        cy.get('[data-test="inventory-item-name"]').click();
+        // Check if the item with the specified name and price exists
+        cy.get('[data-test="inventory-item-name"]').contains('Sauce Labs Backpack');
+        cy.get('[data-test="inventory-item-price"]').contains('$29.99');
+        cy.log('Item = Sauce Labs Backpack, price = $29.99 ');
+        cy.get('[data-test="shopping-cart-link"]').click();
+        //Checkout
+        cy.get('#checkout').click();
+        cy.get('[data-test="firstName"]').type('testname');
+        cy.get('[data-test="lastName"]').type('testlname');
+        cy.get('[data-test="postalCode"]').type('1234');
+        cy.get('#continue').click();
+        cy.url().should('include', '/checkout-step-two.html');
+        //Order successful done
+        cy.get('[data-test="finish"]').click();
+        cy.get('[data-test="back-to-products"]').click();
+        cy.get('#react-burger-menu-btn').click();
+        cy.log('Logout the site');
+        cy.get('#logout_sidebar_link').contains('Logout').click();
+
+    });
+
     it('Visits the shopsite and buy item', () => {
 
         cy.visit('https://www.saucedemo.com/');
@@ -82,9 +157,9 @@ describe('My First Test', () => {
         //Logout
         cy.get('#react-burger-menu-btn').click();
         cy.get('[data-test="logout-sidebar-link"]').click();
-            
-        });
-})
 
+
+    });
+})
 
 //Start command => npx cypress open
